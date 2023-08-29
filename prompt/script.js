af9309ce3b3eeed860f953d52344b8fca899256d
Chiyang001 <91011415+Chiyang001@users.noreply.github.com>
2023-08-29 19:48:51 +0800

    Update script.js

--- prompt/script.js
+++ prompt/script.js
@@ -7,3 +7,25 @@ tabs.forEach(tab => {
     tab.classList.toggle('active');
   });
 });
+
+
+// 获取所有的复制按钮
+const copyButtons = document.querySelectorAll('.copy-button');
+
+// 遍历复制按钮，为每个按钮添加点击事件
+copyButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const content = button.parentNode.textContent; // 获取父元素的文本内容
+    copyToClipboard(content.trim()); // 复制文本内容到剪贴板
+  });
+});
+
+// 复制文本到剪贴板的函数
+function copyToClipboard(text) {
+  const textarea = document.createElement('textarea');
+  textarea.value = text;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textarea);
+}
