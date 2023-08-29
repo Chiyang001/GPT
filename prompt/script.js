d285e934c629dd3191a969907fa6fd77fe143a47
Chiyang001 <91011415+Chiyang001@users.noreply.github.com>
2023-08-29 19:29:45 +0800

    Add files via upload

---
+++ prompt/script.js
@@ -0,0 +1,9 @@
+const tabs = document.querySelectorAll('.tab');
+
+tabs.forEach(tab => {
+  const title = tab.querySelector('.tab-title');
+  
+  title.addEventListener('click', () => {
+    tab.classList.toggle('active');
+  });
+});
