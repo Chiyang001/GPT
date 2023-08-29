a8fb9ec0bcd9923a279198a10276c11086967ab6
Chiyang001 <91011415+Chiyang001@users.noreply.github.com>
2023-08-29 19:56:52 +0800

    Update script.js

--- prompt/script.js
+++ prompt/script.js
@@ -9,18 +9,13 @@ tabs.forEach(tab => {
 });
 
 
-// 获取所有的复制按钮
-const copyButtons = document.querySelectorAll('.copy-button');
+const clipboard = new ClipboardJS('.copy-button');
 
-// 遍历复制按钮，为每个按钮添加点击事件
-copyButtons.forEach(button => {
-  button.addEventListener('click', async () => {
-    const content = button.parentNode.textContent.trim();
-    try {
-      await navigator.clipboard.writeText(content);
-      console.log('内容已成功复制到剪贴板');
-    } catch (error) {
-      console.error('复制到剪贴板时出错:', error);
-    }
-  });
+clipboard.on('success', function(event) {
+  console.log('内容已成功复制到剪贴板');
+  event.clearSelection();
+});
+
+clipboard.on('error', function(event) {
+  console.error('复制到剪贴板时出错:', event.action);
 });
